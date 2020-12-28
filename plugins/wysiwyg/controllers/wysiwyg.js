7e751e7edaf5c0e568e66806e50b8ac3056d5514
Renato Oliveira <oliversieto@gmail.com>
2020-12-28 19:31:26 -0400

    chore: configuraçlão do plugin

--- plugins/wysiwyg/controllers/wysiwyg.js
+++ plugins/wysiwyg/controllers/wysiwyg.js
@@ -14,12 +14,5 @@ module.exports = {
    * @return {Object}
    */
 
-  index: async (ctx) => {
-    // Add your own logic here.
-
-    // Send 200 `ok`
-    ctx.send({
-      message: 'ok'
-    });
-  }
+  index: async () => {}
 };
