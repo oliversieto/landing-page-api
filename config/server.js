15058e892444b07de8a1af9d1edc98222c662df2
Renato Oliveira <oliversieto@gmail.com>
2021-02-15 22:39:36 -0400

    refactor: atualização das configurações

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '033db3091c2c1042713172c7e7c51140'),
+      secret: env('ADMIN_JWT_SECRET'),
     },
   },
 });
